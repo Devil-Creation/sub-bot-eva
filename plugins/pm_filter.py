df24415d658cbb682a69e1df23fc51b14ae90d85
Devil-Creation <91359456+Devil-Creation@users.noreply.github.com>
2022-01-11 10:37:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -28,6 +28,105 @@ SPELL_CHECK = {}
 
 
 
+@Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming)
+async def filter(client, message):       
+    if message.text.startswith("/"):
+        return
+    if AUTH_CHANNEL:
+        invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+        try:
+            user = await client.get_chat_member(int(AUTH_CHANNEL), message.from_user.id)
+            if user.status == "kicked":
+                await client.send_message(
+                    chat_id=message.from_user.id,
+                    text="Sorry Sir, You are Banned to use me.",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True
+                )
+                return
+        except UserNotParticipant:
+            await client.send_message(
+                chat_id=message.from_user.id,
+                text="**Please Join My Updates Channel to use this Bot!**",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            await client.send_message(
+                chat_id=message.from_user.id,
+                text="Something went Wrong.",
+                parse_mode="markdown",
+                disable_web_page_preview=True
+            )
+            return
+    if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
+        return
+    if 2 < len(message.text) < 100:    
+        btn = []
+        search = message.text
+        files = await get_filter_results(query=search)
+        if files:
+            for file in files:
+                file_id = file.file_id
+                filename = f"[{get_size(file.file_size)}] {file.file_name}"
+                btn.append(
+                    [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
+                    )
+        else:
+            await client.send_sticker(chat_id=message.from_user.id, sticker='CAACAgUAAxkBAALaq2Gs9PizUzl1UfT24IWBuFJPlF9VAAMDAAJ0ZEFWEua6AAHCwAH6IgQ')
+            return
+
+        if not btn:
+            return
+
+        if len(btn) > 10: 
+            btns = list(split_list(btn, 10)) 
+            keyword = f"{message.chat.id}-{message.message_id}"
+            BUTTONS[keyword] = {
+                "total" : len(btns),
+                "buttons" : btns
+            }
+        else:
+            buttons = btn
+            buttons.append(
+                [InlineKeyboardButton(text="📜 Pages 1/1",callback_data="pages")]
+            )
+            poster=None
+            if API_KEY:
+                poster=await get_poster(search)
+            if poster:
+                await message.reply_photo(photo=poster, caption=f"<b>🍿 මෙන්න ඔයා හොයපු {search} උපසිරැසිය</b> \n\n🥴 <i>ආ! ඹය පින්තූරෙ ගැන වැඩිය හිතන්න එපා ; හරියන උපසිරැසිය තිබ්බොත් download කරගන්න \n<b>දන්නැද්ද හිටපු ගමන් ඩාං වෙනවා ඩුං වෙනවා</b>. . . </i>", reply_markup=InlineKeyboardMarkup(buttons))
+
+            else:
+                await message.reply_text(f"<b>🍿 මෙන්න ඔයා හොයපු {search} උපසිරැසිය ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            return
+
+        data = BUTTONS[keyword]
+        buttons = data['buttons'][0].copy()
+
+        buttons.append(
+            [InlineKeyboardButton(text="NEXT ⏩",callback_data=f"next_0_{keyword}")]
+        )    
+        buttons.append(
+            [InlineKeyboardButton(text=f"📃 Pages 1/{data['total']}",callback_data="pages")]
+        )
+        poster=None
+        if API_KEY:
+            poster=await get_poster(search)
+        if poster:
+            await message.reply_photo(photo=poster, caption=f"<b>🍿 මෙන්න ඔයා හොයපු {search} උපසිරැසිය</b> \n\n★ ඹයා හොයන උපසිරැසිය තිබ්බෙ නැත්තන්  යට තියන NEXT බටන් එක ඹබලා බලන්න \n\n🥴 <i>ආ! ඹය පින්තූරෙ ගැන වැඩිය හිතන්න එපා ; හරියන උපසිරැසිය තිබ්බොත් download කරගන්න \n<b>දන්නැද්ද හිටපු ගමන් ඩාං වෙනවා ඩුං වෙනවා</b>. . . </i>", reply_markup=InlineKeyboardMarkup(buttons))
+        else:
+            await message.reply_text(f"<b>🍿 මෙන්න ඔයා හොයපු {search} උපසිරැසිය</b> \n\n★ ඹයා හොයන උපසිරැසිය තිබ්බෙ නැත්තන්  යට තියන NEXT බටන් එක ඹබලා බලන්න", reply_markup=InlineKeyboardMarkup(buttons))
+
+
+
 @Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
 async def give_filter(client,message):
     k = await manual_filters(client, message)
